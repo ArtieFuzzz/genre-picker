67b899b0771a037b6ace1a4203f7fb995a581e2f
♥ ArtieFuzzz ♥ <48877462+ArtieFuzzz@users.noreply.github.com>
2023-07-23 13:04:14 +1000

    fix: Fix max width

--- src/components/GenrePicker.svelte
+++ src/components/GenrePicker.svelte
@@ -26,11 +26,15 @@
   })
 </script>
 
-{#if !genre}
-  <p class="text-xl font-mono mt-0">Picking a genre...</p>
-{:else}
-  <p class="text-xl font-mono mt-0">
-    The genre I have chosen is... <span class="font-extrabold">{genre}</span>
-  </p>
-  <button class="rounded-md" on:click={async () => pick()}>Re-pick</button>
-{/if}
+<div
+  class="flex flex-col items-center justify-center w-screen max-w-[21rem] md:max-w-xl"
+>
+  {#if !genre}
+    <p class="text-[1rem] font-mono mt-0">Picking a genre...</p>
+  {:else}
+    <p class="text-[1rem] font-mono mt-0">
+      The genre I have chosen is... <span class="font-extrabold">{genre}</span>
+    </p>
+    <button class="rounded-md" on:click={async () => pick()}>Re-pick</button>
+  {/if}
+</div>
